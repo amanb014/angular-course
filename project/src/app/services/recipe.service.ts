9a88bf9ca1a9790c0f0fb5fb417c3c6548eccbc6
Aman Bhimani <amanb014@gmail.com>
2018-03-26 15:13:41 -0400

    recipe lists all ingredeints needed for it

--- project/src/app/services/recipe.service.ts
+++ project/src/app/services/recipe.service.ts
@@ -1,10 +1,20 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './../shared/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 export class RecipeService {
   
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Recipe', 'This is some absurdly long description for a recipe. Not really that long though.', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Pizza',
+      'This is simply the best way to make pizza here.',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Cheese', 4), new Ingredient('Bread', 1), new Ingredient('Vegetables', 5)]),
+
+      new Recipe(
+        'Salad',
+        'This is simply the best way to make salad here.',
+        'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [new Ingredient('Spinach', 4), new Ingredient('Tomatoes', 5), new Ingredient('Green Peppers', 5)]),
   ]
 
   recipeSelected = new EventEmitter<Recipe>()
